89b8e7a166fdce165ce3cc6a5cd7f582481a4f94
Meshach <116318961+Meshach88@users.noreply.github.com>
2024-06-03 12:11:36 +0100

    Added menu items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories items = {menuItems}/>
+        <Menu items = {menuItems}/>
+      </section>
+    </main>
+  );
 }
 
 export default App;
